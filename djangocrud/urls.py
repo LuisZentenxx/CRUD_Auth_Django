680209e2d38baaf3caa7a37299ffb41051c9c98d
LuisZentenxx <luis.zenteno02@inacapmail.cl>
2023-06-05 23:35:10 -0400

    creacion formulario

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -20,5 +20,6 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.message)
+    path('', views.home, name="home"),
+    path('registrar/', views.signup, name="signup"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
-def message(reques):
-    return HttpResponse('<h1>Hola</h1>')
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    return render(request, 'registro.html', {
+        'form' : UserCreationForm
+    } )
